e390ea910f97fad50c04d1dc5b6c2834b8c58fda
ton1czech <tonyasek007@gmail.com>
2023-12-02 16:52:35 +0100

    🎁NEW: theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { Navbar } from '@/components/navbar/navbar'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +15,21 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={cn('antialiased scroll-smooth', inter.className)}>
-        {children}
+      <body
+        className={cn(
+          'antialiased scroll-smooth min-h-[300vh]',
+          inter.className
+        )}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          //   disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
