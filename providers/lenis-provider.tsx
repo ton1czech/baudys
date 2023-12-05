efe2c22f8ed896c8168a749b895933d8358cf69e
ton1czech <tonyasek007@gmail.com>
2023-12-05 17:40:28 +0100

    👌IMPROVE: lenis provider

--- providers/lenis-provider.tsx
+++ providers/lenis-provider.tsx
@@ -1,19 +1,21 @@
 'use client'
 
 import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
 
 export const LenisProvider = () => {
-  const lenis = new Lenis()
+  useEffect(() => {
+    const lenis = new Lenis()
 
-  lenis.on('scroll', (e: any) => {
-    console.log(e)
-  })
+    lenis.on('scroll', (e: any) => {
+      console.log(e)
+    })
 
-  function raf(time: any) {
-    lenis.raf(time)
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
     requestAnimationFrame(raf)
-  }
-
-  requestAnimationFrame(raf)
-  return <></>
+  }, [])
 }
