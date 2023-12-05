a070fd25308eaaac9edfceccbd8195be90650d3d
ton1czech <tonyasek007@gmail.com>
2023-12-05 17:34:42 +0100

    👌IMPROVE: mist

--- components/mist.tsx
+++ components/mist.tsx
@@ -1,8 +1,8 @@
 export const Mist = () => {
   return (
     <div>
-      <div className='absolute top-0 left-0 w-full h-full mist opacity-60 dark:opacity-30 pointer-events-none max-lg:hidden' />
-      <div className='mobile-mist absolute left-32 top-0 w-full h-full scale-150 opacity-50 dark:opacity-20 pointer-events-none lg:hidden' />
+      <div className='absolute top-0 left-0 w-full h-full mist opacity-60 dark:opacity-30 max-lg:hidden' />
+      <div className='mobile-mist absolute left-1/2 -translate-x-1/2 top-0 w-full h-full scale-150 opacity-50 dark:opacity-20 lg:hidden' />
     </div>
   )
 }
