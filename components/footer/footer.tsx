8b390ad75bed03425d76dcf5f5f752da070f6d53
ton1czech <tonyasek007@gmail.com>
2023-12-05 16:22:47 +0100

    🎁NEW: footer back to top button

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { useLanguage } from '@/store/useLanguage'
 import { Container } from '../container'
 import Link from 'next/link'
 import { FooterIcon } from './footer-icon'
-import { Github, Instagram, Linkedin } from 'lucide-react'
+import { ArrowUp, Github, Instagram, Linkedin } from 'lucide-react'
 import { useCursor } from '@/store/useCursor'
 
 export const Footer = () => {
@@ -12,9 +12,27 @@ export const Footer = () => {
   const { setIsHovering, setIsNotHovering } = useCursor()
 
   return (
-    <footer className='pt-32 pb-12 border-t dark:border-zinc-700/50 border-zinc-500/50'>
+    <footer className='pt-8 lg:pt-12 pb-12 border-t dark:border-zinc-700/50 border-zinc-500/50'>
       <Container>
-        <div className='flex justify-center gap-4 mb-20'>
+        <div className='flex justify-center mb-40'>
+          <button
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+              })
+            }}
+            className='flex items-center gap-1 colorful-text text-sm font-semibold relative after:w-full after:h-[3px] after:bg-purple-500 after:-bottom-1 after:left-0 after:right-0 after:z-10 after:absolute after:opacity-0 after:scale-x-0 after:hover:scale-x-100 after:hover:opacity-100 after:transition after:rounded-lg'
+            onMouseEnter={setIsHovering}
+            onMouseLeave={setIsNotHovering}
+          >
+            {language === 'en' && 'Back to top'}
+            {language === 'cs' && 'Zpět na začátek'}
+            <ArrowUp className='h-4 w-4 text-sky-500' />
+          </button>
+        </div>
+
+        <div className='flex justify-center gap-4 mb-14'>
           <div onMouseEnter={setIsHovering} onMouseLeave={setIsNotHovering}>
             <FooterIcon
               name='github'
