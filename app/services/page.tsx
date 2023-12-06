1a8547cbe492ad46534859437a1cd9bfcccf5d66
ton1czech <tonyasek007@gmail.com>
2023-12-06 10:09:00 +0100

    🎁NEW: everything in cz + services page contact

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,22 @@
+'use client'
+
+import { Contact } from '@/components/contact'
 import { Everything } from '@/components/services/everything'
+import { useCursor } from '@/store/useCursor'
+import { useEffect } from 'react'
 
 export default function Page() {
+  const { setIsNotHovering, setType } = useCursor()
+
+  useEffect(() => {
+    setIsNotHovering()
+    setType('')
+  }, [])
+
   return (
     <div className='mt-28 lg:mt-32 mb-20 lg:mb-32'>
       <Everything />
+      <Contact />
     </div>
   )
 }
--- components/services/everything.tsx
+++ components/services/everything.tsx
@@ -4,6 +4,7 @@ import { useLanguage } from '@/store/useLanguage'
 import { Container } from '../container'
 import { Title } from '../title'
 import { EverythingItem } from './everything-item'
+import { motion } from 'framer-motion'
 
 export const Everything = () => {
   const { language } = useLanguage()
@@ -12,7 +13,12 @@ export const Everything = () => {
     <Container>
       <Title label={language === 'en' ? 'Everything I do' : 'Vše, co dělám'} />
 
-      <div className='grid lg:grid-cols-3 gap-10 lg:gap-20'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.3 }}
+        className='grid lg:grid-cols-3 gap-10 lg:gap-20 pb-20 lg:pb-60 xl:pb-80'
+      >
         <EverythingItem
           title={language === 'en' ? 'Engineering' : 'Inženýrství'}
           items={
@@ -24,20 +30,35 @@ export const Everything = () => {
                   'social site',
                   'platform',
                   '3D website',
+                  'web apps',
                 ]
-              : ['', '', '', '', '', '']
+              : [
+                  'front-end',
+                  'eshop',
+                  'rezeverační systém',
+                  'socální síť',
+                  'platforma',
+                  '3D web',
+                  'webové aplikace',
+                ]
           }
         />
         <EverythingItem
-          title={language === 'en' ? 'Management' : ''}
+          title={language === 'en' ? 'Management' : 'Správa'}
           items={
             language === 'en'
               ? ['newsletter', 'emails', 'news', 'hosting', 'analytics reports']
-              : ['', '', '', '', '', '']
+              : [
+                  'newsletter',
+                  'emaily',
+                  'novinky',
+                  'hosting',
+                  'analytické přehledy',
+                ]
           }
         />
         <EverythingItem
-          title={language === 'en' ? 'Add-ons' : ''}
+          title={language === 'en' ? 'Add-ons' : 'Doplňky'}
           items={
             language === 'en'
               ? [
@@ -51,19 +72,36 @@ export const Everything = () => {
                   'chatbot',
                   'database connection',
                   'payment gateway',
+                  'animations',
                 ]
-              : ['', '', '', '', '', '']
+              : [
+                  'newsletter',
+                  'email formulář',
+                  '3D prvky',
+                  'admininstrátorské rozhraní',
+                  'analytika',
+                  'vyskakovací okna',
+                  'CDN integrace',
+                  'chatbot',
+                  'napojení na databázi',
+                  'animace',
+                ]
           }
         />
         <EverythingItem
-          title={language === 'en' ? 'Configuration' : ''}
+          title={language === 'en' ? 'Configuration' : 'Nastavení'}
           items={
             language === 'en'
-              ? ['domain', 'hosting platform', 'search console indexing', 'SEO']
-              : ['', '', '', '', '', '']
+              ? ['domain', 'hosting platform', 'search engines indexing', 'SEO']
+              : [
+                  'doména',
+                  'hostingová platforma',
+                  'indexování ve vyhledávačích',
+                  'SEO',
+                ]
           }
         />
-      </div>
+      </motion.div>
     </Container>
   )
 }
