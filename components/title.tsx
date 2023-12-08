4a7dc03561150872b2719e88a403b0943cd5fd64
ton1czech <tonyasek007@gmail.com>
2023-12-08 10:29:44 +0100

    👌IMPROVE: title py for czech words to be readable

--- components/title.tsx
+++ components/title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 
 export const Title: FC<TitleProps> = ({ label }) => {
   return (
-    <h3 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-center mb-6 xl:mb-10 uppercase'>
+    <h3 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-center mb-6 xl:mb-10 uppercase py-2'>
       {label}
     </h3>
   )
