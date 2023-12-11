2f7c551e58e7f3305778e74a850256223d68f446
ton1czech <tonyasek007@gmail.com>
2023-12-11 18:07:19 +0100

    🎁NEW: tpms page

--- app/services/tpms/page.tsx
+++ app/services/tpms/page.tsx
@@ -1,7 +1,11 @@
+import { Contact } from '@/components/contact'
+import { Process } from '@/components/services/tpms/process'
+
 export default function Page() {
   return (
-    <div>
-      <p>tpms</p>
+    <div className='mt-28 lg:mt-32 mb-20 lg:mb-32'>
+      <Process />
+      <Contact />
     </div>
   )
 }
