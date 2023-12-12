f493c76d5afeee952b38b33067c2f6950aabe509
ton1czech <tonyasek007@gmail.com>
2023-12-12 23:04:53 +0100

    🎁NEW: tpms pictures

--- components/services/tpms/process.tsx
+++ components/services/tpms/process.tsx
@@ -11,21 +11,21 @@ export const Process = () => {
     <Container className='pb-40 lg:pb-60 xl:pb-80 space-y-8 lg:space-y-20 xl:space-y-40'>
       <ProcessItem
         align='right'
-        label={language === 'en' ? 'Installation' : 'Montáž'}
-        body={
-          language === 'en'
-            ? 'Physically fitting smart valves is the first step to optimising your vehicle. With an emphasis on precision and quality workmanship, I will ensure the seamless installation of these valves to maintain the safety and performance of your tires.'
-            : 'Fyzické namontování chytrých ventilků je prvním krokem k optimalizaci vašeho vozidla. S důrazem na preciznost a kvalitu provedení se postarám o bezproblémovou instalaci těchto ventilků, aby byla zachována bezpečnost a výkon vašich pneumatik.'
-        }
-        img='/services/tpms/process/1.webp'
-      />
-      <ProcessItem
-        align='left'
         label={language === 'en' ? 'Programming' : 'Programování'}
         body={
           language === 'en'
             ? 'The programming of smart valves is crucial for their optimal function in a particular vehicle. Using diagnostic tools and expertise, I will perform detailed programming of the valves to ensure they are fully compatible with your vehicle and ensure the correct functioning of the tire pressure monitoring system.'
             : 'Programování chytrých ventilků je klíčové pro jejich optimální funkci v konkrétním vozidle. S využitím diagnostických nástrojů a odborných znalostí provedu detailní programování ventilků tak, aby byly plně kompatibilní s vaším vozidlem a zajistily správné fungování systému sledování tlaku v pneumatikách.'
+        }
+        img='/services/tpms/process/1.webp'
+      />
+      <ProcessItem
+        align='left'
+        label={language === 'en' ? 'Installation' : 'Montáž'}
+        body={
+          language === 'en'
+            ? 'Physically fitting smart valves is the first step to optimising your vehicle. With an emphasis on precision and quality workmanship, I will ensure the seamless installation of these valves to maintain the safety and performance of your tires.'
+            : 'Fyzické namontování chytrých ventilků je prvním krokem k optimalizaci vašeho vozidla. S důrazem na preciznost a kvalitu provedení se postarám o bezproblémovou instalaci těchto ventilků, aby byla zachována bezpečnost a výkon vašich pneumatik.'
         }
         img='/services/tpms/process/2.webp'
       />
