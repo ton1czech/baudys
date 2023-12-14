1613cb0cc3f4339a23828a52350d4d67cf93cbe8
ton1czech <tonyasek007@gmail.com>
2023-12-14 22:31:11 +0100

    👌IMPROVE: typo

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -14,7 +14,7 @@ export const Services = () => {
         body={
           language === 'en'
             ? 'I will bring your visions to life. I responsibly design and build websites that represent you. I provide responsive design, intuitive navigation and aesthetic elements to reach your audience. I will bring your unique stories, ideas and messages to life through a high quality, modern website.'
-            : 'Přinesu vašim vizím život. Zodpovědně navrhuji a vytvářím webové stránky, které vás reprezentují. Zajistím responzivní design, intuitivní navigaci a estetické prvky, abyste oslovili své publikum. Vaše jedinečné příběhy, nápady a poselství přineseu k životu prostřednictvím kvalitního a moderního webu.'
+            : 'Přinesu vašim vizím život. Zodpovědně navrhuji a vytvářím webové stránky, které vás reprezentují. Zajistím responzivní design, intuitivní navigaci a estetické prvky, abyste oslovili své publikum. Vaše jedinečné příběhy, nápady a poselství přinesu k životu prostřednictvím kvalitního a moderního webu.'
         }
         img='/services/web-development/hero.webp'
         href='/services/web-development'
