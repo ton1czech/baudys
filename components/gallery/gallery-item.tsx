f09e181a65245be3b119bba7bed61ced71446787
ton1czech <tonyasek007@gmail.com>
2024-04-22 15:59:27 +0200

    👌IMPROVE: gallery UI changes

--- components/gallery/gallery-item.tsx
+++ components/gallery/gallery-item.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { useCursor } from '@/store/use-cursor'
 import { useLanguage } from '@/store/use-language'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 interface Props {
   item: any
@@ -15,27 +16,33 @@ export const GalleryItem = ({ item }: Props) => {
   const [isHovered, setIsHovered] = useState(false)
 
   return (
-    <Link
-      onMouseEnter={() => {
-        setIsHovering()
-        setIsHovered(true)
-      }}
-      onMouseLeave={() => {
-        setIsNotHovering()
-        setIsHovered(false)
-      }}
-      href={item.href}
-      className='flex flex-col flex-nowrap group relative'
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ delay: 0.3 }}
     >
-      <img
-        src={item.images[0]}
-        className='transition duration-300 rounded-xl'
-        style={{ filter: isHovered ? 'brightness(100%)' : 'brightness(85%)' }}
-      />
-      <p className='text-sm text-zinc-700 dark:text-zinc-300 absolute bottom-2 left-2'>
-        {language === 'en' && item.labelEn}
-        {language === 'cs' && item.labelCs}
-      </p>
-    </Link>
+      <Link
+        onMouseEnter={() => {
+          setIsHovering()
+          setIsHovered(true)
+        }}
+        onMouseLeave={() => {
+          setIsNotHovering()
+          setIsHovered(false)
+        }}
+        href={item.href}
+        className='flex flex-col flex-nowrap group relative'
+      >
+        <img
+          src={item.images[0]}
+          className='transition duration-300 rounded-xl'
+          style={{ filter: isHovered ? 'brightness(100%)' : 'brightness(85%)' }}
+        />
+        <p className='text-sm text-zinc-200 absolute bottom-2 left-2 bg-black/20 p-1 rounded-md'>
+          {language === 'en' && item.labelEn}
+          {language === 'cs' && item.labelCs}
+        </p>
+      </Link>
+    </motion.div>
   )
 }
