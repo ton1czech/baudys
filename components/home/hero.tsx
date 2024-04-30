71bb3f713d0beacc0a8d33dd5f20eb86ce9a455c
ton1czech <tonyasek007@gmail.com>
2024-04-30 13:18:05 +0200

    👌IMPROVE: hero rework WIP

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -11,12 +11,46 @@ export const Hero = () => {
 
   return (
     <motion.div
-      className='overflow-x-hidden min-h-screen relative'
+      className='overflow-x-hidden relative pb-40 lg:pb-60 xl:pb-80'
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.3 }}
     >
-      <Container className='min-h-screen grid place-content-center'>
+      <Container>
+        <div className='relative h-[80vh] w-full mt-28 lg:mt-32 bg-[url(/home/hero.webp)] rounded-3xl bg-cover bg-center'>
+          <div className='absolute inset-0 bg-black/80 rounded-3xl' />
+          <div className='flex justify-center flex-col w-full h-full'>
+            <h4 className='font-semibold md:text-lg lg:text-xl xl:text-2xl text-zinc-500 -mb-2 relative z-10 text-center'>
+              Daniel Anthony Baudyš
+            </h4>
+            <h1 className='text-5xl xl:text-6xl 2xl:text-7xl font-black colorful-text py-2 text-center relative z-10'>
+              <Typewriter
+                options={{
+                  strings:
+                    language === 'en'
+                      ? ['Developer', 'Designer', 'Programmer']
+                      : ['Vývojář', 'Designer', 'Programátor'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>{' '}
+            <div className='flex justify-center'>
+              <div className='flex items-center bg-zinc-100/70 dark:bg-zinc-800/70 px-4 py-2 border border-zinc-500 dark:border-zinc-600 rounded-lg gap-2 mt-2 relative z-10 w-full justify-center sm:w-auto'>
+                <MapPin />
+                {language === 'en' && 'based in Czech Republic'}
+                {language === 'cs' && 'se sídlem v České republice'}
+                <img
+                  src='/flags/cs.webp'
+                  alt='czech republic flag'
+                  className='w-5'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+      {/* <Container className='min-h-screen grid place-content-center'>
         <div>
           <h4 className='font-semibold md:text-lg lg:text-xl xl:text-2xl text-zinc-600 -mb-2 relative z-10 text-center 2xl:text-left'>
             {language === 'en' && <>Hey, my name is Daniel.</>}
@@ -36,18 +70,7 @@ export const Hero = () => {
             />
           </h1>
 
-          <div className='flex justify-center 2xl:justify-end'>
-            <div className='flex items-center bg-zinc-100/70 dark:bg-zinc-800/70 px-4 py-2 border border-zinc-500 dark:border-zinc-600 rounded-lg gap-2 mt-2 relative z-10 w-full justify-center sm:w-auto'>
-              <MapPin />
-              {language === 'en' && 'based in Czech Republic'}
-              {language === 'cs' && 'se sídlem v České republice'}
-              <img
-                src='/flags/cs.webp'
-                alt='czech republic flag'
-                className='w-5'
-              />
-            </div>
-          </div>
+        
 
           {language === 'en' && (
             <p className='max-w-[75ch] mt-20 dark:text-zinc-400 text-zinc-700 text-justify relative z-10'>
@@ -68,7 +91,7 @@ export const Hero = () => {
             </p>
           )}
         </div>
-      </Container>
+      </Container> */}
     </motion.div>
   )
 }
