654cd9f1fb914933ca784f335816a669dca2700d
ton1czech <tonyasek007@gmail.com>
2024-05-21 11:06:21 +0200

    👌IMPROVE: blog card date bg and text color

--- components/blog/card.tsx
+++ components/blog/card.tsx
@@ -12,14 +12,14 @@ export const Card = ({
 }: Props) => {
   return (
     <Link href={`/blog/${slug}`} className='group'>
-      <div className='relative border border-zinc-400/40 rounded-2xl p-4 overflow-hidden'>
+      <div className='relative border border-zinc-400/40 rounded-2xl overflow-hidden aspect-square'>
         <img
           src={image}
           alt={title}
-          className='w-full group-hover:scale-105 transition'
+          className='absolute w-full group-hover:scale-105 transition object-center left-0 -top-1/2'
         />
 
-        <span className='absolute right-3 bottom-3 text-zinc-300 text-xs'>
+        <span className='absolute right-3 bottom-3 text-zinc-100 text-xs bg-black/20 p-1 rounded-md'>
           {new Date(publishDate).toLocaleDateString()}
         </span>
       </div>
@@ -31,7 +31,7 @@ export const Card = ({
             <Link
               key={i}
               href={`/blog/${cat}`}
-              className='bg-violet-700 px-1 rounded-md'
+              className='bg-violet-700 px-2 py-0.5 rounded-md text-zinc-100 text-sm'
             >
               {cat}
             </Link>
