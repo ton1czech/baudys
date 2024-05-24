03b4444373ec0159b9b97a38844a29c94df7b487
ton1czech <tonyasek007@gmail.com>
2024-05-24 11:27:59 +0200

    👌IMPROVE: hero text animation

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -2,12 +2,12 @@
 
 import { Container } from '@/components/container'
 import { useLanguage } from '@/store/use-language'
-import Typewriter from 'typewriter-effect'
 import { MapPin } from 'lucide-react'
 import { motion } from 'framer-motion'
 import { ButtonGradient } from '../ui/button-gradient'
 import Link from 'next/link'
 import { BackgroundBeams } from '../ui/background-beams'
+import { FlipWords } from '../ui/flip-words'
 
 export const Hero = () => {
   const { language } = useLanguage()
@@ -23,23 +23,20 @@ export const Hero = () => {
       <Container>
         <div className='relative h-[80vh] w-full mt-28 lg:mt-32'>
           <div className='flex justify-center flex-col w-full h-full p-4'>
-            <h4 className='md:text-lg lg:text-xl xl:text-2xl text-zinc-500 -mb-2 relative z-10 text-center'>
+            <h4 className='md:text-lg lg:text-xl xl:text-2xl text-zinc-500 -mb-2 text-center'>
               Daniel Anthony Baudyš
             </h4>
-            <h1 className='text-5xl xl:text-6xl 2xl:text-7xl font-black colorful-text py-2 text-center relative z-10'>
-              <Typewriter
-                options={{
-                  strings:
-                    language === 'en'
-                      ? ['Developer', 'Designer', 'Programmer', 'Photographer']
-                      : ['Vývojář', 'Designer', 'Programátor', 'Fotograf'],
-                  autoStart: true,
-                  loop: true,
-                }}
+            <h1 className='text-5xl xl:text-6xl 2xl:text-7xl font-black py-2 text-center text-violet-600'>
+              <FlipWords
+                words={
+                  language === 'en'
+                    ? ['Developer', 'Designer', 'Programmer', 'Photographer']
+                    : ['Vývojář', 'Designer', 'Programátor', 'Fotograf']
+                }
               />
             </h1>
             <div className='flex justify-center'>
-              <div className='flex items-center bg-zinc-800/70 px-4 py-2 border border-zinc-600 rounded-lg gap-2 mt-2 relative z-10 w-full justify-center sm:w-auto text-zinc-400'>
+              <div className='flex items-center bg-zinc-800/70 px-4 py-2 border border-zinc-600 rounded-lg gap-2 mt-2 w-full justify-center sm:w-auto text-zinc-400'>
                 <MapPin size={22} />
                 {language === 'en' && 'based in Czech Republic'}
                 {language === 'cs' && 'se sídlem v České republice'}
@@ -51,14 +48,14 @@ export const Hero = () => {
               </div>
             </div>
             {language === 'en' && (
-              <p className='max-w-[52ch] mt-12 text-zinc-400 text-justify relative mx-auto'>
+              <p className='max-w-[52ch] mt-12 text-zinc-400 text-justify mx-auto'>
                 I am committed to taking you into the digital universe where you
                 will encounter excellence and design combined in a unique
                 harmony.
               </p>
             )}
             {language === 'cs' && (
-              <p className='max-w-[50ch] mt-12 text-zinc-400 text-justify relative mx-auto'>
+              <p className='max-w-[50ch] mt-12 text-zinc-400 text-justify mx-auto'>
                 Jsem odhodlaný zavést vás do digitálního vesmíru, kde se setkáte
                 s excelencí a designem spojeným v jedinečné harmonii.
               </p>
