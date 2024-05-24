8d51a1f39429a7a6accaba0b97c1308031080a4f
ton1czech <tonyasek007@gmail.com>
2024-05-24 15:16:04 +0200

    👌IMPROVE: flip words animation props

--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -56,13 +56,13 @@ export const FlipWords = ({
         }}
         exit={{
           opacity: 0,
-          y: -40,
-          x: 40,
+          y: -100,
+          x: 0,
           filter: 'blur(8px)',
           scale: 2,
           position: 'absolute',
         }}
-        className={cn('z-10 inline-block relative text-left px-2', className)}
+        className={cn('z-10 inline-block relative', className)}
         key={currentWord}
       >
         {currentWord.split('').map((letter, index) => (
