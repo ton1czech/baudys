ca2983c62896f68770c02f43c35ef284ca86b0e0
ton1czech <tonyasek007@gmail.com>
2024-05-24 17:55:47 +0200

    👌IMPROVE: hero section for lightmode

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -27,16 +27,24 @@ export const Hero = () => {
               Daniel Anthony Baudyš
             </h4>
             <h1 className='text-5xl xl:text-6xl 2xl:text-7xl font-black py-2 text-center text-violet-600'>
-              <FlipWords
-                words={
-                  language === 'en'
-                    ? ['Developer', 'Designer', 'Programmer', 'Photographer']
-                    : ['Vývojář', 'Designer', 'Programátor', 'Fotograf']
-                }
-              />
+              {language === 'en' && (
+                <FlipWords
+                  words={[
+                    'Developer',
+                    'Designer',
+                    'Programmer',
+                    'Photographer',
+                  ]}
+                />
+              )}
+              {language === 'cs' && (
+                <FlipWords
+                  words={['Vývojář', 'Designer', 'Programátor', 'Fotograf']}
+                />
+              )}
             </h1>
             <div className='flex justify-center'>
-              <div className='flex items-center bg-zinc-800/70 px-4 py-2 border border-zinc-600 rounded-lg gap-2 mt-2 w-full justify-center sm:w-auto text-zinc-400'>
+              <div className='flex items-center bg-zinc-200/70 dark:bg-zinc-800/70 px-4 py-2 border border-zinc-400 dark:border-zinc-600 rounded-lg gap-2 mt-2 w-full justify-center sm:w-auto text-zinc-600 dark:text-zinc-400'>
                 <MapPin size={22} />
                 {language === 'en' && 'based in Czech Republic'}
                 {language === 'cs' && 'se sídlem v České republice'}
@@ -48,21 +56,21 @@ export const Hero = () => {
               </div>
             </div>
             {language === 'en' && (
-              <p className='max-w-[52ch] mt-12 text-zinc-400 text-justify mx-auto'>
+              <p className='max-w-[52ch] mt-12 text-zinc-600 dark:text-zinc-400 text-justify mx-auto'>
                 I am committed to taking you into the digital universe where you
                 will encounter excellence and design combined in a unique
                 harmony.
               </p>
             )}
             {language === 'cs' && (
-              <p className='max-w-[50ch] mt-12 text-zinc-400 text-justify mx-auto'>
+              <p className='max-w-[50ch] mt-12 text-zinc-600 dark:text-zinc-400 text-justify mx-auto'>
                 Jsem odhodlaný zavést vás do digitálního vesmíru, kde se setkáte
                 s excelencí a designem spojeným v jedinečné harmonii.
               </p>
             )}
             <div className='flex justify-center mt-28'>
               <Link href='/services'>
-                <ButtonGradient className='py-2 px-6 font-semibold text-lg'>
+                <ButtonGradient className='py-2 px-6 font-semibold text-lg bg-zinc-200 dark:bg-zinc-900 text-black dark:text-white'>
                   {language === 'en' && 'Take Action!'}
                   {language === 'cs' && 'Jednej!'}
                 </ButtonGradient>
