84366da6fe3e3ee55f9971cd477f6f4688ab2cb2
ton1czech <tonyasek007@gmail.com>
2024-10-30 10:39:11 +0100

    👌IMPROVE: hero redesign

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -4,87 +4,105 @@ import { Container } from '@/components/container'
 import { useLanguage } from '@/store/use-language'
 import { MapPin } from 'lucide-react'
 import Clock from 'react-live-clock'
+import { motion } from 'framer-motion'
 
 export const Hero = () => {
   const { language } = useLanguage()
 
   return (
     <section>
-      <Container className='flex flex-col gap-1 md:grid md:grid-cols-2 md:gap-5'>
-        <div className='order-2 mt-2 md:order-1'>
-          <img src='/home/hero.webp' alt='me' className='rounded-xl' />
-          <div className='mt-3 flex items-center gap-2 rounded-xl border border-zinc-200/10 bg-zinc-800/80 p-2 md:hidden'>
-            <div className='flex items-center gap-1'>
-              <MapPin className='size-5' />
-              {language === 'cs' && 'Plzeňský kraj, Česká republika'}
-              {language === 'en' && 'Pilsen region, Czech republic'}
-            </div>
-            <span className='text-xs text-zinc-300'>
-              (
-              <Clock
-                format={'HH:mm:ss'}
-                ticking={true}
-                timezone={'EU/Prague'}
+      <Container className='flex flex-col items-center'>
+        <div className='rounded-xl border border-zinc-200/10 bg-zinc-800/80'>
+          <div className='grid sm:grid-cols-2'>
+            <div className='relative'>
+              <img
+                src='/home/hero.webp'
+                alt='me'
+                className='rounded-t-xl sm:rounded-l-xl sm:rounded-r-none'
               />
-              )
-            </span>
+
+              <p className='absolute left-2 top-2 rounded-xl border-zinc-200/10 bg-zinc-800/80 p-2 px-3 text-sm md:text-base'>
+                {language === 'cs' && 'Ahoj, já jsem Daniel'}
+                {language === 'en' && "Hi, I'm Daniel"}
+              </p>
+
+              <div className='absolute bottom-2 right-2 flex items-center gap-2 rounded-xl border border-zinc-200/10 bg-zinc-800/80 p-2 text-xs sm:text-sm md:text-base'>
+                <div className='flex items-center gap-1'>
+                  <MapPin className='size-5' />
+                  {language === 'cs' && 'Plzeňský kraj, Česká republika'}
+                  {language === 'en' && 'Pilsen region, Czech republic'}
+                </div>
+                <span>
+                  (
+                  <Clock
+                    format={'HH:mm:ss'}
+                    ticking={true}
+                    timezone={'Europe/Prague'}
+                  />
+                  )
+                </span>
+              </div>
+            </div>
+
+            <div className='flex flex-col p-4'>
+              <div className='space-y-1 text-2xl font-bold md:space-y-2 md:text-3xl lg:text-5xl 2xl:text-6xl'>
+                <p className='flex items-center justify-between gap-4 text-fuchsia-200'>
+                  {language === 'cs' && 'Vývojář'}
+                  {language === 'en' && 'Developer'}
+                  <span className='rounded-md border border-zinc-200/10 bg-fuchsia-500/20 p-1 text-sm text-fuchsia-500 md:p-2 md:text-base'>
+                    5 {language === 'cs' && 'let'}
+                    {language === 'en' && 'years'}
+                  </span>
+                </p>
+
+                <p className='flex items-center justify-between gap-4 text-green-200'>
+                  {language === 'cs' && 'Fotograf'}
+                  {language === 'en' && 'Photographer'}
+                  <span className='rounded-md border border-zinc-200/10 bg-green-500/20 p-1 text-sm text-green-500 md:p-2 md:text-base'>
+                    9 {language === 'cs' && 'let'}
+                    {language === 'en' && 'years'}
+                  </span>
+                </p>
+
+                <p className='flex items-center justify-between gap-4 text-sky-200'>
+                  {language === 'cs' && 'Designér'}
+                  {language === 'en' && 'Designer'}
+                  <span className='rounded-md border border-zinc-200/10 bg-sky-500/20 p-1 text-sm text-sky-500 md:p-2 md:text-base'>
+                    3 {language === 'cs' && 'roky'}
+                    {language === 'en' && 'years'}
+                  </span>
+                </p>
+
+                <p className='flex items-center justify-between gap-4 text-yellow-200'>
+                  {language === 'cs' && 'Tvůrce'}
+                  {language === 'en' && 'Creator'}
+                  <span className='rounded-md border border-zinc-200/10 bg-yellow-500/20 p-1 text-sm text-yellow-500 md:p-2 md:text-base'>
+                    2 {language === 'cs' && 'roky'}
+                    {language === 'en' && 'years'}
+                  </span>
+                </p>
+
+                <p className='flex items-center justify-between gap-4 text-indigo-200'>
+                  {language === 'cs' && 'Programátor'}
+                  {language === 'en' && 'Programmer'}
+                  <span className='rounded-md border border-zinc-200/10 bg-indigo-500/20 p-1 text-sm text-indigo-500 md:p-2 md:text-base'>
+                    4 {language === 'cs' && 'roky'}
+                    {language === 'en' && 'years'}
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className='order-1 flex flex-col justify-between md:order-2'>
-          <div>
-            <h1 className='text-zinc-300'>
-              {language === 'cs' && 'Ahoj, já jsem Daniel'}
-              {language === 'en' && "Hi, I'm Daniel"}
-            </h1>
-
-            <div className='space-y-1 text-2xl font-bold md:space-y-2 lg:text-4xl 2xl:text-5xl'>
-              <p className='flex items-center gap-4'>
-                {language === 'cs' && 'Programátor'}
-                {language === 'en' && 'Programmer'}
-                <span className='rounded-md border border-zinc-200/10 bg-fuchsia-500/20 p-1 text-sm text-fuchsia-500 md:p-2 md:text-base'>
-                  4 {language === 'cs' && 'roky'}
-                  {language === 'en' && 'years'}
-                </span>
-              </p>
-
-              <p className='flex items-center gap-4'>
-                {language === 'cs' && 'Fotograf'}
-                {language === 'en' && 'Photographer'}
-                <span className='rounded-md border border-zinc-200/10 bg-green-500/20 p-1 text-sm text-green-500 md:p-2 md:text-base'>
-                  9 {language === 'cs' && 'let'}
-                  {language === 'en' && 'years'}
-                </span>
-              </p>
-
-              <p className='flex items-center gap-4'>
-                {language === 'cs' && 'Designér'}
-                {language === 'en' && 'Designer'}
-                <span className='rounded-md border border-zinc-200/10 bg-sky-500/20 p-1 text-sm text-sky-500 md:p-2 md:text-base'>
-                  3 {language === 'cs' && 'roky'}
-                  {language === 'en' && 'years'}
-                </span>
-              </p>
-            </div>
-          </div>
-
-          <div className='hidden items-center gap-2 rounded-xl border border-zinc-200/10 bg-zinc-800/80 p-2 md:flex'>
-            <div className='flex items-center gap-1'>
-              <MapPin className='size-5' />
-              {language === 'cs' && 'Plzeňský kraj, Česká republika'}
-              {language === 'en' && 'Pilsen region, Czech republic'}
-            </div>
-            <span>
-              (
-              <Clock
-                format={'HH:mm:ss'}
-                ticking={true}
-                timezone={'EU/Prague'}
-              />
-              )
-            </span>
-          </div>
-        </div>
+        <p className='mt-4 text-justify md:text-lg xl:text-2xl'>
+          Pomáhám projektům ožít s jasnou vizí, funkčností a stylem. Mým cílem
+          je propojit
+          <span className='text-sky-500'> krásu</span>,{' '}
+          <span className='text-fuchsia-500'>účel</span> a
+          <span className='text-green-500'> efektivitu </span>
+          ve všech digitálních zážitcích.
+        </p>
       </Container>
     </section>
   )
