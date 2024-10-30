fe04578b3013b070807291cfe69a1b0797c057db
ton1czech <tonyasek007@gmail.com>
2024-10-30 10:42:54 +0100

    🎁NEW: animate photo component

--- components/gallery/photo.tsx
+++ components/gallery/photo.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Cursor } from '../cursor'
 import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
+import { motion } from 'framer-motion'
 
 interface PhotoProps {
   photo: string
@@ -7,19 +10,25 @@ interface PhotoProps {
 
 export const Photo = ({ photo }: PhotoProps) => {
   return (
-    <Dialog>
-      <DialogTrigger>
-        <Cursor type='photo'>
-          <img src={photo} alt='photo' className='rounded-lg' />
-        </Cursor>
-      </DialogTrigger>
-      <DialogContent className='max-h-[90vh] max-w-[95vw]'>
-        <img
-          src={photo}
-          alt='photo'
-          className='max-h-[90vh] max-w-[95vw] rounded-lg'
-        />
-      </DialogContent>
-    </Dialog>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ delay: 0.3 }}
+    >
+      <Dialog>
+        <DialogTrigger>
+          <Cursor type='photo'>
+            <img src={photo} alt='photo' className='rounded-lg' />
+          </Cursor>
+        </DialogTrigger>
+        <DialogContent className='max-h-[90vh] max-w-[95vw]'>
+          <img
+            src={photo}
+            alt='photo'
+            className='max-h-[90vh] max-w-[95vw] rounded-lg'
+          />
+        </DialogContent>
+      </Dialog>
+    </motion.div>
   )
 }
