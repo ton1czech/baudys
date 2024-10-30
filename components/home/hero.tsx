5bca64414a9c4473ecae89e4bbe07072c8e07794
ton1czech <tonyasek007@gmail.com>
2024-10-30 10:46:31 +0100

    🎁NEW: hero animations

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -15,18 +15,31 @@ export const Hero = () => {
         <div className='rounded-xl border border-zinc-200/10 bg-zinc-800/80'>
           <div className='grid sm:grid-cols-2'>
             <div className='relative'>
-              <img
+              <motion.img
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
                 src='/home/hero.webp'
                 alt='me'
                 className='rounded-t-xl sm:rounded-l-xl sm:rounded-r-none'
               />
 
-              <p className='absolute left-2 top-2 rounded-xl border-zinc-200/10 bg-zinc-800/80 p-2 px-3 text-sm md:text-base'>
+              <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+                className='absolute left-2 top-2 rounded-xl border-zinc-200/10 bg-zinc-800/80 p-2 px-3 text-sm md:text-base'
+              >
                 {language === 'cs' && 'Ahoj, já jsem Daniel'}
                 {language === 'en' && "Hi, I'm Daniel"}
-              </p>
+              </motion.p>
 
-              <div className='absolute bottom-2 right-2 flex items-center gap-2 rounded-xl border border-zinc-200/10 bg-zinc-800/80 p-2 text-xs sm:text-sm md:text-base'>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+                className='absolute bottom-2 right-2 flex items-center gap-2 rounded-xl border border-zinc-200/10 bg-zinc-800/80 p-2 text-xs sm:text-sm md:text-base'
+              >
                 <div className='flex items-center gap-1'>
                   <MapPin className='size-5' />
                   {language === 'cs' && 'Plzeňský kraj, Česká republika'}
@@ -41,68 +54,98 @@ export const Hero = () => {
                   />
                   )
                 </span>
-              </div>
+              </motion.div>
             </div>
 
             <div className='flex flex-col p-4'>
               <div className='space-y-1 text-2xl font-bold md:space-y-2 md:text-3xl lg:text-5xl 2xl:text-6xl'>
-                <p className='flex items-center justify-between gap-4 text-fuchsia-200'>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className='flex items-center justify-between gap-4 text-fuchsia-200'
+                >
                   {language === 'cs' && 'Vývojář'}
                   {language === 'en' && 'Developer'}
                   <span className='rounded-md border border-zinc-200/10 bg-fuchsia-500/20 p-1 text-sm text-fuchsia-500 md:p-2 md:text-base'>
                     5 {language === 'cs' && 'let'}
                     {language === 'en' && 'years'}
                   </span>
-                </p>
+                </motion.p>
 
-                <p className='flex items-center justify-between gap-4 text-green-200'>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className='flex items-center justify-between gap-4 text-green-200'
+                >
                   {language === 'cs' && 'Fotograf'}
                   {language === 'en' && 'Photographer'}
                   <span className='rounded-md border border-zinc-200/10 bg-green-500/20 p-1 text-sm text-green-500 md:p-2 md:text-base'>
                     9 {language === 'cs' && 'let'}
                     {language === 'en' && 'years'}
                   </span>
-                </p>
+                </motion.p>
 
-                <p className='flex items-center justify-between gap-4 text-sky-200'>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className='flex items-center justify-between gap-4 text-sky-200'
+                >
                   {language === 'cs' && 'Designér'}
                   {language === 'en' && 'Designer'}
                   <span className='rounded-md border border-zinc-200/10 bg-sky-500/20 p-1 text-sm text-sky-500 md:p-2 md:text-base'>
                     3 {language === 'cs' && 'roky'}
                     {language === 'en' && 'years'}
                   </span>
-                </p>
+                </motion.p>
 
-                <p className='flex items-center justify-between gap-4 text-yellow-200'>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className='flex items-center justify-between gap-4 text-yellow-200'
+                >
                   {language === 'cs' && 'Tvůrce'}
                   {language === 'en' && 'Creator'}
                   <span className='rounded-md border border-zinc-200/10 bg-yellow-500/20 p-1 text-sm text-yellow-500 md:p-2 md:text-base'>
                     2 {language === 'cs' && 'roky'}
                     {language === 'en' && 'years'}
                   </span>
-                </p>
+                </motion.p>
 
-                <p className='flex items-center justify-between gap-4 text-indigo-200'>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className='flex items-center justify-between gap-4 text-indigo-200'
+                >
                   {language === 'cs' && 'Programátor'}
                   {language === 'en' && 'Programmer'}
                   <span className='rounded-md border border-zinc-200/10 bg-indigo-500/20 p-1 text-sm text-indigo-500 md:p-2 md:text-base'>
                     4 {language === 'cs' && 'roky'}
                     {language === 'en' && 'years'}
                   </span>
-                </p>
+                </motion.p>
               </div>
             </div>
           </div>
         </div>
 
-        <p className='mt-4 text-justify md:text-lg xl:text-2xl'>
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.3 }}
+          className='mt-4 text-justify md:text-lg xl:text-2xl'
+        >
           Pomáhám projektům ožít s jasnou vizí, funkčností a stylem. Mým cílem
           je propojit
           <span className='text-sky-500'> krásu</span>,{' '}
           <span className='text-fuchsia-500'>účel</span> a
           <span className='text-green-500'> efektivitu </span>
           ve všech digitálních zážitcích.
-        </p>
+        </motion.p>
       </Container>
     </section>
   )
